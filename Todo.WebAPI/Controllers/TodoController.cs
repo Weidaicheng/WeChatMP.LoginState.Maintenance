7920c7905deb5c865891bafa32ad06ee276fc014
Weidaicheng <wdcdavyc@outlook.com>
2017-09-13 20:25:26 +0800

    添加获取过期Todo方法，提取调用接口获取OpenId方法，添加 添加Todo

--- Todo.WebAPI/Controllers/TodoController.cs
+++ Todo.WebAPI/Controllers/TodoController.cs
@@ -33,6 +33,17 @@ namespace Todo.WebAPI.Controllers
         }
         #endregion
 
+        #region 登录服务
+        private ResponseResult<string> getOpenId(RequestBase model)
+        {
+            IRestRequest request = new RestRequest("/GetOpenId", Method.POST);
+            request.AddJsonBody(model);
+
+            IRestResponse response = _client.Execute(request);
+            return JsonConvert.DeserializeObject<ResponseResult<string>>(response.Content);
+        }
+        #endregion
+
         #region 用户
         /// <summary>
         /// 用户是否已注册
@@ -179,6 +190,7 @@ namespace Todo.WebAPI.Controllers
         #endregion
 
         #region Todo
+        #region 查询Todo
         /// <summary>
         /// 获取今日Todo
         /// </summary>
@@ -190,11 +202,7 @@ namespace Todo.WebAPI.Controllers
             try
             {
                 //登录接口使用model.UserId获取OpenId
-                IRestRequest request = new RestRequest("/GetOpenId", Method.POST);
-                request.AddJsonBody(model);
-
-                IRestResponse response = _client.Execute(request);
-                var result = JsonConvert.DeserializeObject<ResponseResult<OpenId>>(response.Content);
+                var result = getOpenId(model);
                 if(result.ErrCode != 0)
                 {
                     logger.Error(result.ErrMsg);
@@ -207,7 +215,7 @@ namespace Todo.WebAPI.Controllers
                 }
 
                 List<Models.Todo> todos = (from t in _todoContext.Todos
-                                           where t.TodoUser.OpenId == result.Data.openid && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) == 0
+                                           where t.TodoUser.OpenId == result.Data && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) == 0
                                            select t).ToList();
                 List<TodoViewModel> todoVMs = new List<TodoViewModel>();
                 foreach(var item in todos)
@@ -255,11 +263,7 @@ namespace Todo.WebAPI.Controllers
             try
             {
                 //登录接口使用model.UserId获取OpenId
-                IRestRequest request = new RestRequest("/GetOpenId", Method.POST);
-                request.AddJsonBody(model);
-
-                IRestResponse response = _client.Execute(request);
-                var result = JsonConvert.DeserializeObject<ResponseResult<OpenId>>(response.Content);
+                var result = getOpenId(model);
                 if (result.ErrCode != 0)
                 {
                     logger.Error(result.ErrMsg);
@@ -272,7 +276,7 @@ namespace Todo.WebAPI.Controllers
                 }
 
                 List<Models.Todo> todos = (from t in _todoContext.Todos
-                                           where t.TodoUser.OpenId == result.Data.openid && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 1 && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) <= 3
+                                           where t.TodoUser.OpenId == result.Data && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 1 && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) <= 3
                                            select t).ToList();
                 List<TodoViewModel> todoVMs = new List<TodoViewModel>();
                 foreach (var item in todos)
@@ -320,11 +324,7 @@ namespace Todo.WebAPI.Controllers
             try
             {
                 //登录接口使用model.UserId获取OpenId
-                IRestRequest request = new RestRequest("/GetOpenId", Method.POST);
-                request.AddJsonBody(model);
-
-                IRestResponse response = _client.Execute(request);
-                var result = JsonConvert.DeserializeObject<ResponseResult<OpenId>>(response.Content);
+                var result = getOpenId(model);
                 if (result.ErrCode != 0)
                 {
                     logger.Error(result.ErrMsg);
@@ -337,7 +337,7 @@ namespace Todo.WebAPI.Controllers
                 }
 
                 List<Models.Todo> todos = (from t in _todoContext.Todos
-                                           where t.TodoUser.OpenId == result.Data.openid && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 3 && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) <= 7
+                                           where t.TodoUser.OpenId == result.Data && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 3 && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) <= 7
                                            select t).ToList();
                 List<TodoViewModel> todoVMs = new List<TodoViewModel>();
                 foreach (var item in todos)
@@ -385,11 +385,7 @@ namespace Todo.WebAPI.Controllers
             try
             {
                 //登录接口使用model.UserId获取OpenId
-                IRestRequest request = new RestRequest("/GetOpenId", Method.POST);
-                request.AddJsonBody(model);
-
-                IRestResponse response = _client.Execute(request);
-                var result = JsonConvert.DeserializeObject<ResponseResult<OpenId>>(response.Content);
+                var result = getOpenId(model);
                 if (result.ErrCode != 0)
                 {
                     logger.Error(result.ErrMsg);
@@ -402,7 +398,7 @@ namespace Todo.WebAPI.Controllers
                 }
 
                 List<Models.Todo> todos = (from t in _todoContext.Todos
-                                           where t.TodoUser.OpenId == result.Data.openid && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 7
+                                           where t.TodoUser.OpenId == result.Data && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) > 7
                                            select t).ToList();
                 List<TodoViewModel> todoVMs = new List<TodoViewModel>();
                 foreach (var item in todos)
@@ -438,6 +434,126 @@ namespace Todo.WebAPI.Controllers
                 };
             }
         }
+
+        /// <summary>
+        /// 获取过期Todo
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ResponseResult<List<TodoViewModel>> GetExpiredTodos([FromBody]RequestBase model)
+        {
+            try
+            {
+                //登录接口使用model.UserId获取OpenId
+                var result = getOpenId(model);
+                if (result.ErrCode != 0)
+                {
+                    logger.Error(result.ErrMsg);
+                    return new ResponseResult<List<TodoViewModel>>()
+                    {
+                        ErrCode = 1002,
+                        ErrMsg = result.ErrMsg,
+                        Data = null
+                    };
+                }
+
+                List<Models.Todo> todos = (from t in _todoContext.Todos
+                                           where t.TodoUser.OpenId == result.Data && DbFunctions.DiffDays(t.AlertTime, DateTime.Now) < 0
+                                           select t).ToList();
+                List<TodoViewModel> todoVMs = new List<TodoViewModel>();
+                foreach (var item in todos)
+                {
+                    todoVMs.Add(new TodoViewModel()
+                    {
+                        AlertBeforeMinutes = item.AlertBeforeMinutes,
+                        AlertTime = item.AlertTime,
+                        Content = item.Content,
+                        CreateTime = item.CreateTime,
+                        Title = item.Title,
+                        TodoId = item.TodoId,
+                        TodoUserId = item.TodoUser.TodoUserId,
+                        UpdateTime = item.UpdateTime,
+                        UseAlert = item.UseAlert
+                    });
+                }
+                return new ResponseResult<List<TodoViewModel>>()
+                {
+                    ErrCode = 0,
+                    ErrMsg = "success",
+                    Data = todoVMs
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                return new ResponseResult<List<TodoViewModel>>()
+                {
+                    ErrCode = 1001,
+                    ErrMsg = ex.Message,
+                    Data = null
+                };
+            }
+        }
+        #endregion
+
+        #region 添加/编辑/删除
+        public ResponseResult<Guid?> AddTodo(TodoAddRequest model)
+        {
+            try
+            {
+                var result = getOpenId(model);
+                if (result.ErrCode != 0)
+                {
+                    logger.Error(result.ErrMsg);
+                    return new ResponseResult<Guid?>()
+                    {
+                        ErrCode = 1002,
+                        ErrMsg = result.ErrMsg,
+                        Data = null
+                    };
+                }
+
+                if(string.IsNullOrEmpty(model.Title))
+                {
+                    throw new ArgumentNullException("标题为空");
+                }
+
+                var user = (from u in _todoContext.TodoUsers
+                            where u.OpenId == result.Data
+                            select u).FirstOrDefault();
+                var todo = new Models.Todo()
+                {
+                    TodoId = Guid.NewGuid(),
+                    Title = model.Title,
+                    Content = model.Content,
+                    AlertTime = model.AlertTime,
+                    AlertBeforeMinutes = model.AlertBeforeMinutes,
+                    UseAlert = model.UseAlert,
+                    CreateTime = DateTime.Now,
+                    TodoUser = user
+                };
+                _todoContext.Todos.Add(todo);
+                _todoContext.SaveChanges();
+
+                return new ResponseResult<Guid?>()
+                {
+                    ErrCode = 0,
+                    ErrMsg = "success",
+                    Data = todo.TodoId
+                };
+            }
+            catch(Exception ex)
+            {
+                return new ResponseResult<Guid?>()
+                {
+                    ErrCode = 1001,
+                    ErrMsg = ex.Message,
+                    Data = null
+                };
+            }
+        }
+        #endregion
         #endregion
     }
 }
---
+++ Todo.WebAPI/Models/TodoAddRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Todo.WebAPI.Models
+{
+    public class TodoAddRequest : RequestBase
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool UseAlert { get; set; }
+        public DateTime? AlertTime { get; set; }
+        public int AlertBeforeMinutes { get; set; }
+    }
+}
